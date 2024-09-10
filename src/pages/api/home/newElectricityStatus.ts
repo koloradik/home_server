2ce7448e61f65b02678b325bd84e6f9af9040d93
koloradik <ivanrobobox@gmail.com>
2024-09-10 08:58:44 +0300

    fix

---
+++ src/pages/api/home/newElectricityStatus.ts
@@ -0,0 +1,60 @@
+import { prisma } from "@/lib/db";
+import { NextApiRequest, NextApiResponse } from "next";
+
+let timeout: NodeJS.Timeout;
+
+function startTimeout() {
+  if (timeout) clearTimeout(timeout);
+
+  timeout = setTimeout(async () => {
+    await prisma.home.update({
+      where: {
+        id: "819c8269-f307-4d18-9ea9-4f2a9b2f1d03",
+      },
+      data: {
+        electricity: false,
+      },
+    });
+    console.log("Таймер сработал: статус электричества изменён на false");
+  }, 120000);
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ msg: "method not allowed" });
+  }
+
+  startTimeout();
+
+  const body = req.body as {
+    id: string;
+    electricity: boolean;
+  };
+
+  if (!body.id || typeof body.electricity !== "boolean") {
+    console.log("Ошибка: некорректные данные", body);
+    return res.status(400).json({ msg: "Bad request" });
+  }
+
+  const home = await prisma.home.findUnique({
+    where: { id: body.id },
+  });
+
+  if (!home) {
+    return res.status(404).json({ msg: "not found" });
+  }
+
+  const NewElectricityStatus = await prisma.home.update({
+    where: {
+      id: body.id,
+    },
+    data: {
+      electricity: body.electricity,
+    },
+  });
+
+  return res.status(200).json({ NewElectricityStatus });
+}
